b3bac6c72b000831930653c6e3a8a11a86edc0c0
utsav <autsav73@gmail.com>
2020-05-10 10:18:50 +0545

    migration and adminlte

---
+++ database/migrations/2020_05_09_140121_create_transaction_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+           $table->id();
+           $table->foreignId('sender_id');
+           $table->foreignId('receiver_id');
+           $table->timestamp('payment_date')->nullable();
+           $table->timestamp('estimated_delivery_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+}
