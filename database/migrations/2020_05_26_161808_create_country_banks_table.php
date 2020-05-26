d61b0e834de61fff988c9282bbf92b198508779b
utsav <autsav73@gmail.com>
2020-05-26 23:29:37 +0545

    county_bank, countries, banks table migration

---
+++ database/migrations/2020_05_26_161808_create_country_banks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountryBanksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('country_banks', function (Blueprint $table) {
+            $table->id();
+             $table->unsignedBigInteger('county_id');
+             $table->foreign('county_id')->references('id')->on('countries');
+
+            $table->unsignedBigInteger('bank_id');
+            $table->foreign('bank_id')->references('id')->on('banks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('country_banks');
+    }
+}
